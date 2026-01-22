856c670c677f541dd917a894eb82b0fce26b931c
chucki01 <mirandes2004@hotmail.com>
2026-01-22 12:37:25 +0100

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
-export const SPOTIFY_CLIENT_ID = "";
-export const REDIRECT_URL = "http://localhost:4173/";
+export const SPOTIFY_CLIENT_ID = "264ad6d82e0d43c384c092b394a69890";
+export const REDIRECT_URL = "https://blind-song-scanner-nine.vercel.app";
